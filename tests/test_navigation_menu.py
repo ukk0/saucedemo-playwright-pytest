91fd422e880cfaed9d2d3fe8e40aeb3d8de0b2ac
ukk0 <sami.ukk91@gmail.com>
2025-11-01 20:43:00 +0100

    use correct page in navigation

--- tests/test_navigation_menu.py
+++ tests/test_navigation_menu.py
@@ -1,15 +1,15 @@
 import pytest
 
 
-def test_check_side_menu_contents_and_functionality(inventory_page, pages):
-    inventory_page.navigate_to_page(url=pages["INVENTORY_PAGE"], title="Products")
-    inventory_page.open_side_menu()
+def test_check_side_menu_contents_and_functionality(navigation_page, pages):
+    navigation_page.navigate_to_page(url=pages["INVENTORY_PAGE"], title="Products")
+    navigation_page.open_side_menu()
 
-    assert inventory_page.menu_all_items_button.is_visible()
-    assert inventory_page.menu_about_button.is_visible()
-    assert inventory_page.menu_logout_button.is_visible()
-    assert inventory_page.menu_reset_app_button.is_visible()
-    inventory_page.close_side_menu()
+    assert navigation_page.menu_all_items_button.is_visible()
+    assert navigation_page.menu_about_button.is_visible()
+    assert navigation_page.menu_logout_button.is_visible()
+    assert navigation_page.menu_reset_app_button.is_visible()
+    navigation_page.close_side_menu()
 
 
 @pytest.mark.parametrize(
@@ -19,18 +19,18 @@ def test_check_side_menu_contents_and_functionality(inventory_page, pages):
         "Logout"
     ]
 )
-def test_side_menu_navigation(inventory_page, pages, menu_option):
-    inventory_page.navigate_to_page(url=pages["INVENTORY_PAGE"], title="Products")
-    inventory_page.open_side_menu()
+def test_side_menu_navigation(navigation_page, pages, menu_option):
+    navigation_page.navigate_to_page(url=pages["INVENTORY_PAGE"], title="Products")
+    navigation_page.open_side_menu()
 
     if menu_option == "All Items":
-        inventory_page.menu_all_items_button.click()
-        inventory_page.current_page_should_be(expected_url=pages["INVENTORY_PAGE"])
+        navigation_page.menu_all_items_button.click()
+        navigation_page.current_page_should_be(expected_url=pages["INVENTORY_PAGE"])
 
     elif menu_option == "About":
-        inventory_page.menu_about_button.click()
-        inventory_page.current_page_should_be(expected_url=pages["ABOUT_PAGE"])
+        navigation_page.menu_about_button.click()
+        navigation_page.current_page_should_be(expected_url=pages["ABOUT_PAGE"])
 
     elif menu_option == "Logout":
-        inventory_page.menu_logout_button.click()
-        inventory_page.current_page_should_be(expected_url=pages["LOGIN_PAGE"])
+        navigation_page.menu_logout_button.click()
+        navigation_page.current_page_should_be(expected_url=pages["LOGIN_PAGE"])
